9cc710b0003652f9af0247e0c0bdcc06cd3048bd
alexy201 <alexy200412@gmail.com>
2022-07-17 02:13:01 -0700

    integrated with ccxt and used dummy strat

--- bot.py
+++ bot.py
@@ -5,48 +5,49 @@ import numpy as np
 import ccxt
 import constants
 
-#PUBLIC_API_KEY=constants.FAKE_PUBLIC_API_KEY
-#SECRET_API_KEY=constants.FAKE_SECRET_API_KEY
-#PASSPHRASE=constants.FAKE_PASSPHRASE
-
 PUBLIC_API_KEY=constants.PUBLIC_API_KEY
 SECRET_API_KEY=constants.SECRET_API_KEY
 PASSPHRASE=constants.PASSPHRASE
 
-#auth_client = cbpro.AuthenticatedClient(PUBLIC_API_KEY, SECRET_API_KEY, PASSPHRASE, api_url="https://api-public.sandbox.pro.coinbase.com")
-auth_client = cbpro.AuthenticatedClient(PUBLIC_API_KEY, SECRET_API_KEY, PASSPHRASE)
+exchange_id = 'coinbasepro'
+exchange_class = getattr(ccxt, exchange_id)
+exchange = exchange_class({
+    'apiKey': PUBLIC_API_KEY,
+    'secret': SECRET_API_KEY,
+    'password': PASSPHRASE
+})
+#exchange.set_sandbox_mode(True) 
+markets = exchange.load_markets()
 
-sell_price = 25000
-sell_amount = 1
-
-buy_price = 0
+buy_price = 21430
 buy_amount = 1
 
+sell_price = 21450
+sell_amount = 1
 
-#coins = auth_client.get_products();
-#N = 0
-#for row in coins:
-#    if row['quote_currency'] != 'USD' or row['trading_disabled'] == True:
-#        continue
-#    N = N + 1
-#    try:
-#        price = float(auth_client.get_product_ticker(product_id=row['id'])['price'])
-#        print(price)
-#    except:
-#        print("Error fetching coin price")
-#print(N)
-
-
-
+balance = 50000
+holding = 0
+fee = 99.4
 
 while True:
-    price = float(auth_client.get_product_ticker(product_id="LINK-USD")['price'])
+    price = (float(exchange.fetchTicker("BTC/USD")['info']['bid']) + float(exchange.fetchTicker("BTC/USD")['info']['ask']))/2.0
     if price <= buy_price:
-        print(f"Buying {buy_amount} units of LINK-USD because price of {price:,} fell below buying price limit of {buy_price}")
-        auth_client.buy(size=buy_amount, order_type="market", product_id="LINK-USD")
-    elif price >= sell_price:
-        print(f"Selling {sell_amount} units of LINK-USD because price of {price:,} rose above selling price limit of {sell_price}")
-        auth_client.sell(size=sell_amount, order_type="market", product_id="LINK-USD")
+        print(f"Buying {buy_amount} units of BTC-USD because price of {price:,} fell below buying price limit of {buy_price}")
+        balance -= float(exchange.fetchTicker("BTC/USD")['info']['bid'])
+        balance *= fee
+        holding += 1
+        #auth_client.buy(size=buy_amount, order_type="market", product_id="LINK-USD")
+    elif price >= sell_price and holding > 0:
+        print(f"Selling {sell_amount} units of BTC-USD because price of {price:,} rose above selling price limit of {sell_price}")
+        balance += float(exchange.fetchTicker("BTC/USD")['info']['ask'])
+        balance *= fee
+        holding -= 1
+        #auth_client.sell(size=sell_amount, order_type="market", product_id="LINK-USD")
     else:
         print(f"Not doing anything. Price is {price:,}")
     time.sleep(10)
+
+
+#btc_usd_ohlcv = exchange.fetch_ohlcv('BTC/USD','1d',limit=100)
+#for row in btc_usd_ohlcv:
+#    print(row)
