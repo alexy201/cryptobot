4cc3cf1fcb557d8020c8af018b79542ff853e762
alexy201 <alexy200412@gmail.com>
2022-07-16 22:50:23 -0700

    stupid strat

--- bot.py
+++ bot.py
@@ -10,32 +10,22 @@ PUBLIC_API_KEY=constants.PUBLIC_API_KEY
 SECRET_API_KEY=constants.SECRET_API_KEY
 PASSPHRASE=constants.PASSPHRASE
 
-public_client = cbpro.PublicClient()
-result = public_client.get_product_24hr_stats('BTC-USD')
-
 auth_client = cbpro.AuthenticatedClient(PUBLIC_API_KEY, SECRET_API_KEY, PASSPHRASE)
 
-print(auth_client.buy(price="0", size="2.1", order_type="limit", product_id="ETH-USD")) #limit
-auth_client.place_limit_order(product_id="BTC-USD", side="buy", price="10.0",size="2")
-print(auth_client.buy(size="10", order_type="market", product_id="ETH-USD")) #market
-
-print(auth_client.sell(price="20000000.00", size="10", order_type="limit", product_id="BTC-USD"))
-print(auth_client.sell(size="10", order_type="market", product_id="BTC-USD")) #market
-print(auth_client.cancel_all(product_id="BTC_USD"))
-
-auth_client.get_orders()
-
-
-sell_price = 30000
+sell_price = 25000
 sell_amount = 0.3
 
-buy_price = 25000
+buy_price = 20000
 buy_amount = 0.2
 
 while True:
     price = float(auth_client.get_product_ticker(product_id="BTC-USD")['price'])
     if price <= buy_price:
-        print("BUYING BTC")
+        print(f"Buying {buy_amount} units of BTC-USD because price of {price:,} fell below buying price limit of {buy_price}")
         auth_client.buy(size=buy_amount, order_type="market", product_id="BTC-USD")
     elif price >= sell_price:
-        print("SELLING BTC")
+        print(f"Selling {sell_amount} units of BTC-USD because price of {price:,} rose above selling price limit of {sell_price}")
+        auth_client.sell(size=sell_amount, order_type="market", product_id="BTC-USD")
+    else:
+        print(f"Not doing anything. Price is {price:,}")
+    time.sleep(10)
